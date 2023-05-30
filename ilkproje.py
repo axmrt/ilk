ee15aaec9123089818c09988d2f50e1e017caeec
axmrt <135058267+axmrt@users.noreply.github.com>
2023-05-30 21:23:08 +0300

    ilkproje.py
    ...

--- ilkproje.py
+++ ilkproje.py
@@ -1,1 +1,13 @@
+print("LOL,Cringe,Pc,Hub,Ethernet,Mod")
 
+
+modern_kelime_soz = {"LOL":"kahkaha atmak", "cringe":"utandırıcı", "PC":"kişisel bilgisayar","hub":"merkez","ethernet":"internet kablosu","mod":"yönetici"}
+
+
+
+cevap = input("hangi kelimenin anlamını öğenmek istiyorsun?")
+
+if cevap in modern_kelime_soz:
+    print(modern_kelime_soz[cevap])
+else:
+    print("kelime sozlukte yok")
